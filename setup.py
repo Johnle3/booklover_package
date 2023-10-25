85e64be0300191ed97243ca8f4866e839dcd6a18
johnle987 <80999777+johnle987@users.noreply.github.com>
2023-10-24 20:20:33 -0400

    completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name= 'Demo ',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/Johnle3/my_project.git',
+    author='John Le',
+    author_email='jkl4cxj@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
